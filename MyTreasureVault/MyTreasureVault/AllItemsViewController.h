926c5dd66c134de83a78ebb33e6b0e1accdaa1de
Elijah Freestone <elojahfreestone@Elijahs-MacBook-Pro.local>
2014-02-11 15:23:05 -0700

    Removed spaces from project name and added landscape ipad splash

---
+++ MyTreasureVault/MyTreasureVault/AllItemsViewController.h
@@ -0,0 +1,18 @@
+// Elijah Freestone
+// ADP1 1402
+// My Treasure Vault
+// February 10th, 2014
+
+//
+//  SecondViewController.h
+//  My Treasure Vault
+//
+//  Created by Elijah Freestone on 2/10/14.
+//  Copyright (c) 2014 Elijah Freestone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
---
+++ MyTreasureVault/MyTreasureVault/AppDelegate.h
@@ -0,0 +1,20 @@
+// Elijah Freestone
+// ADP1 1402
+// My Treasure Vault
+// February 10th, 2014
+
+//
+//  AppDelegate.h
+//  My Treasure Vault
+//
+//  Created by Elijah Freestone on 2/10/14.
+//  Copyright (c) 2014 Elijah Freestone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
