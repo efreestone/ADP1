cf2d69d0872cd5870489fbb48d0da5b73040c05b
Elijah Freestone <elojahfreestone@Elijahs-MacBook-Pro.local>
2014-02-11 16:54:22 -0700

    Added custom cell class. Not set up yet

---
+++ MyTreasureVault/MyTreasureVault/CustomCell.h
@@ -0,0 +1,13 @@
+//
+//  CustomCell.h
+//  MyTreasureVault
+//
+//  Created by Elijah Freestone on 2/11/14.
+//  Copyright (c) 2014 Elijah Freestone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomCell : UITableViewCell
+
+@end
