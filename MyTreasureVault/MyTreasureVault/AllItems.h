cf71425e2beffd53ad52c1c698e201673d9703f8
Elijah Freestone <elojahfreestone@Elijahs-MacBook-Pro.local>
2014-02-11 20:27:10 -0700

    Added all items custom object

---
+++ MyTreasureVault/MyTreasureVault/AllItems.h
@@ -0,0 +1,13 @@
+//
+//  AllItems.h
+//  MyTreasureVault
+//
+//  Created by Elijah Freestone on 2/11/14.
+//  Copyright (c) 2014 Elijah Freestone. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AllItems : NSObject
+
+@end
---
+++ MyTreasureVault/MyTreasureVault/AllItems.m
@@ -0,0 +1,13 @@
+//
+//  AllItems.m
+//  MyTreasureVault
+//
+//  Created by Elijah Freestone on 2/11/14.
+//  Copyright (c) 2014 Elijah Freestone. All rights reserved.
+//
+
+#import "AllItems.h"
+
+@implementation AllItems
+
+@end
