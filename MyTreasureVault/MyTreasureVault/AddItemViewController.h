378204351fece1fa966a6e657c1eeb20bbb5ff40
Elijah Freestone <elojahfreestone@Elijahs-MacBook-Pro.local>
2014-02-12 17:05:43 -0700

    Added add item view controller

---
+++ MyTreasureVault/MyTreasureVault/AddItemViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddItemViewController.h
+//  MyTreasureVault
+//
+//  Created by Elijah Freestone on 2/12/14.
+//  Copyright (c) 2014 Elijah Freestone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddItemViewController : UIViewController
+
+@end
---
+++ MyTreasureVault/MyTreasureVault/AddItemViewController.m
@@ -0,0 +1,38 @@
+//
+//  AddItemViewController.m
+//  MyTreasureVault
+//
+//  Created by Elijah Freestone on 2/12/14.
+//  Copyright (c) 2014 Elijah Freestone. All rights reserved.
+//
+
+#import "AddItemViewController.h"
+
+@interface AddItemViewController ()
+
+@end
+
+@implementation AddItemViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
