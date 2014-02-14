00eede00e3a03f51bb4f66af7a7ec80581f00c3b
Elijah Freestone <elojahfreestone@Elijahs-MacBook-Pro.local>
2014-02-13 18:27:12 -0700

    Added sign in view

---
+++ MyTreasureVault/MyTreasureVault/SignInViewController.h
@@ -0,0 +1,13 @@
+//
+//  SignInViewController.h
+//  MyTreasureVault
+//
+//  Created by Elijah Freestone on 2/13/14.
+//  Copyright (c) 2014 Elijah Freestone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SignInViewController : UIViewController
+
+@end
---
+++ MyTreasureVault/MyTreasureVault/SignInViewController.m
@@ -0,0 +1,38 @@
+//
+//  SignInViewController.m
+//  MyTreasureVault
+//
+//  Created by Elijah Freestone on 2/13/14.
+//  Copyright (c) 2014 Elijah Freestone. All rights reserved.
+//
+
+#import "SignInViewController.h"
+
+@interface SignInViewController ()
+
+@end
+
+@implementation SignInViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
