2b017d9a58973defbf037112020df3bf2c529102
Elijah Freestone <elojahfreestone@Elijahs-MacBook-Pro.local>
2014-02-17 14:37:53 -0700

    Created basic framework for week 3 to use core data. iPhone storyboard is started, iPad not.

---
+++ MyTreasureVaultV2/MyTreasureVaultV2/AllItemsViewController.h
@@ -0,0 +1,13 @@
+//
+//  AllItemsViewController.h
+//  MyTreasureVaultV2
+//
+//  Created by Elijah Freestone on 2/17/14.
+//  Copyright (c) 2014 Elijah Freestone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AllItemsViewController : UIViewController
+
+@end
---
+++ MyTreasureVaultV2/MyTreasureVaultV2/SettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.h
+//  MyTreasureVaultV2
+//
+//  Created by Elijah Freestone on 2/17/14.
+//  Copyright (c) 2014 Elijah Freestone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UIViewController
+
+@end
