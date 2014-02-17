edce4c840a28fe12deb3857736afdef5e1eed730
Elijah Freestone <elojahfreestone@Elijahs-MacBook-Pro.local>
2014-02-17 15:44:34 -0700

    Copied code from week 2 for recent and all items objects

--- MyTreasureVaultV2/MyTreasureVaultV2/RecentItems.h
+++ MyTreasureVaultV2/MyTreasureVaultV2/RecentItems.h
@@ -1,3 +1,9 @@
+// Elijah Freestone
+// ADP1 1402
+// Week 3
+// My Treasure Vault Version 2
+// February 15th, 2014
+
 //
 //  RecentItems.h
 //  MyTreasureVaultV2
@@ -9,5 +15,14 @@
 #import <Foundation/Foundation.h>
 
 @interface RecentItems : NSObject
+
+//Declare strings for item info
+@property (nonatomic, copy) UIImage *imageOne;
+@property (nonatomic, copy) NSString *itemMake;
+@property (nonatomic, copy) NSString *itemModel;
+@property (nonatomic, copy) NSString *itemSerial;
+@property (nonatomic, copy) NSString *itemDetails;
+@property (nonatomic, copy) NSString *itemCost;
+@property (nonatomic, copy) NSString *dateAdded;
 
 @end
--- MyTreasureVaultV2/MyTreasureVaultV2/RecentItems.m
+++ MyTreasureVaultV2/MyTreasureVaultV2/RecentItems.m
@@ -1,3 +1,9 @@
+// Elijah Freestone
+// ADP1 1402
+// Week 3
+// My Treasure Vault Version 2
+// February 15th, 2014
+
 //
 //  RecentItems.m
 //  MyTreasureVaultV2
@@ -9,5 +15,8 @@
 #import "RecentItems.h"
 
 @implementation RecentItems
+
+//Synthesize image/strings for getter/setter
+@synthesize imageOne, itemMake, itemModel, itemSerial, itemDetails, itemCost, dateAdded;
 
 @end
