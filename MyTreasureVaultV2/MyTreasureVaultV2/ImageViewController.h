367c355f3c97eae3d7476f2be6eb17c837c3840a
Elijah Freestone <elojahfreestone@Elijahs-MacBook-Pro.local>
2014-02-17 16:17:16 -0700

    Added image view controller

---
+++ MyTreasureVaultV2/MyTreasureVaultV2/ImageViewController.h
@@ -0,0 +1,13 @@
+//
+//  ImageViewController.h
+//  MyTreasureVaultV2
+//
+//  Created by Elijah Freestone on 2/17/14.
+//  Copyright (c) 2014 Elijah Freestone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImageViewController : UIViewController
+
+@end
---
+++ MyTreasureVaultV2/MyTreasureVaultV2/ImageViewController.m
@@ -0,0 +1,38 @@
+//
+//  ImageViewController.m
+//  MyTreasureVaultV2
+//
+//  Created by Elijah Freestone on 2/17/14.
+//  Copyright (c) 2014 Elijah Freestone. All rights reserved.
+//
+
+#import "ImageViewController.h"
+
+@interface ImageViewController ()
+
+@end
+
+@implementation ImageViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
