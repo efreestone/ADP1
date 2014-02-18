abc398d5b46dd51a055f2bd60c30755b56ad0a80
Elijah Freestone <elojahfreestone@Elijahs-MacBook-Pro.local>
2014-02-18 15:25:50 -0700

    Added Items Core Data subclass

---
+++ MyTreasureVaultV2/MyTreasureVaultV2/Items.h
@@ -0,0 +1,23 @@
+//
+//  Items.h
+//  MyTreasureVaultV2
+//
+//  Created by Elijah Freestone on 2/18/14.
+//  Copyright (c) 2014 Elijah Freestone. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface Items : NSManagedObject
+
+@property (nonatomic, retain) NSString * cost;
+@property (nonatomic, retain) NSString * details;
+@property (nonatomic, retain) NSString * make;
+@property (nonatomic, retain) NSString * model;
+@property (nonatomic, retain) NSString * serial;
+@property (nonatomic, retain) NSDate * dateAdded;
+@property (nonatomic, retain) NSString * formattedDate;
+
+@end
---
+++ MyTreasureVaultV2/MyTreasureVaultV2/Items.m
@@ -0,0 +1,22 @@
+//
+//  Items.m
+//  MyTreasureVaultV2
+//
+//  Created by Elijah Freestone on 2/18/14.
+//  Copyright (c) 2014 Elijah Freestone. All rights reserved.
+//
+
+#import "Items.h"
+
+
+@implementation Items
+
+@dynamic cost;
+@dynamic details;
+@dynamic make;
+@dynamic model;
+@dynamic serial;
+@dynamic dateAdded;
+@dynamic formattedDate;
+
+@end
