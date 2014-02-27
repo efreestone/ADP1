0e5c5a05a033b9624547b8d35580cf8481fdfa86
Elijah Freestone <elojahfreestone@Elijahs-MacBook-Pro.local>
2014-02-27 12:12:12 -0700

    Set up sign up subclass and added new background/field backgrounds

---
+++ My Treasure Vault/My Treasure Vault/CustomPFSignUpViewController.h
@@ -0,0 +1,13 @@
+//
+//  CustomPFSignUpViewController.h
+//  My Treasure Vault
+//
+//  Created by Elijah Freestone on 2/27/14.
+//  Copyright (c) 2014 Elijah Freestone. All rights reserved.
+//
+
+#import <Parse/Parse.h>
+
+@interface CustomPFSignUpViewController : PFSignUpViewController
+
+@end
---
+++ My Treasure Vault/My Treasure Vault/CustomPFSignUpViewController.m
@@ -0,0 +1,38 @@
+//
+//  CustomPFSignUpViewController.m
+//  My Treasure Vault
+//
+//  Created by Elijah Freestone on 2/27/14.
+//  Copyright (c) 2014 Elijah Freestone. All rights reserved.
+//
+
+#import "CustomPFSignUpViewController.h"
+
+@interface CustomPFSignUpViewController ()
+
+@end
+
+@implementation CustomPFSignUpViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
