d2270a6b79c664af30362e5e4baa9e38fd80fcf1
Elijah Freestone <elojahfreestone@Elijahs-MacBook-Pro.local>
2014-02-26 22:25:07 -0700

    Working on overriding the Parse login screen. iPad done, working on iPhone and then logins for both next

---
+++ My Treasure Vault/My Treasure Vault/CustomPFLoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  CustomPFLoginViewController.h
+//  My Treasure Vault
+//
+//  Created by Elijah Freestone on 2/26/14.
+//  Copyright (c) 2014 Elijah Freestone. All rights reserved.
+//
+
+#import <Parse/Parse.h>
+
+@interface CustomPFLoginViewController : PFLogInViewController
+
+@end
---
+++ My Treasure Vault/My Treasure Vault/CustomPFLoginViewController.m
@@ -0,0 +1,38 @@
+//
+//  CustomPFLoginViewController.m
+//  My Treasure Vault
+//
+//  Created by Elijah Freestone on 2/26/14.
+//  Copyright (c) 2014 Elijah Freestone. All rights reserved.
+//
+
+#import "CustomPFLoginViewController.h"
+
+@interface CustomPFLoginViewController ()
+
+@end
+
+@implementation CustomPFLoginViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
